7db750311641dc0da0b7d2163c8af9763d8a51bc
Vítor Norton <contato@vtnorton.com>
2016-04-11 17:13:18 -0300

    Removed Multilanguage App Toolkit

---
+++ brazil/rio/Information.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+
+namespace ProjectRio
+{
+    public class Information
+    {
+        public static string W10Version = Version.Win10Rio;
+        public static string W8Version = Version.Win10Rio;
+        public static string AdApplicationId = Ads.Douai[0];
+        public static string AdAdUnitId = Ads.Douai[1];
+    }
+}
--- brazil/rio/Views/Help.xaml.cs
+++ brazil/rio/Views/Help.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using Core.Shared;
-using ProjectRio.Code;
+using CoreWin10.Code;
 using System;
 using Windows.System;
 using Windows.UI.Xaml;
