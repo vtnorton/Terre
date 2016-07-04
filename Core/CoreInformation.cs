fb61005ea6875d4e556f4d791920faa5c44b8026
Vítor Norton <norton@vtnorton.com>
2016-07-04 18:55:32 -0300

    #Change: Version number

--- Core/CoreInformation.cs
+++ Core/CoreInformation.cs
@@ -8,10 +8,16 @@ namespace Core
 {
     public class CoreInformation
     {
+        //Open command prompt
+        //git rev-list --all --count
+
         public static int CodeLines = 189;
         public static int Colors = 85;
-        public static string Target = " ";
-        public static string Build = 6 + Target;
+        public static int RevisonNumber = 18;
+        public static string BuildCode = "65";
+        public static string Targat = " ";
+        public static string Build = RevisonNumber + "." + BuildCode + Targat;
+        
 
         #region PATHS
         //public static string PathColors = "\\Data\\colors.txt";
--- Core/Version.cs
+++ Core/Version.cs
@@ -2,7 +2,7 @@
 {
     public class Version
     {
-        public static string Core = "1.1." + CoreInformation.Build;
+        public static string Core = "1.2." + CoreInformation.BuildCode;
 
         #region WEB
         public static string Vtnorton = "1.0.0";
@@ -10,52 +10,25 @@
         #endregion
         
         #region WINDOWS 10
-        public static string Win10LasVegas = "2.1.0" + CoreInformation.Target;
-        public static string Win10Pasadena = "2.1.7" + CoreInformation.Target;
-        public static string Win10Lyon = "2.3.0" + CoreInformation.Target;
-        public static string Win10Douai = "2.1.0" + CoreInformation.Target;
-        public static string Win10Nice = "2.1.0" + CoreInformation.Target;
-        public static string Win10Miame = "1.0.0" + CoreInformation.Target;
-        public static string Win10Rio = "1.1.5" + CoreInformation.Target;
-        public static string Win10Teresopolis = "2.0.0" + CoreInformation.Target;
+        public static string Win10LasVegas = "2.1." + CoreInformation.Build + " _discontinued_";
+        public static string Win10Pasadena = "2.1." + CoreInformation.Build + " _discontinued_";
+        public static string Win10Lyon = "2.3." + CoreInformation.Build;
+        public static string Win10Douai = "2.1." + CoreInformation.Build;
+        public static string Win10Nice = "2.1." + CoreInformation.Build;
+        public static string Win10Miame = "1.0." + CoreInformation.Build;
+        public static string Win10Rio = "1.1." + CoreInformation.Build + " _discontinued_";
+        public static string Win10Teresopolis = "2.0." + CoreInformation.Build;
         #endregion
 
         #region WINDOWS 8.1
-        public static string Win8LasVegas = "1.1.0" + CoreInformation.Target;
-        public static string Win8Pasadena = "1.1.0" + CoreInformation.Target;
-        public static string Win8Lyon = "2.2.0" + CoreInformation.Target;
-        public static string Win8Douai = "1.1.0" + CoreInformation.Target;
-        public static string Win8Nice = "1.1.0" + CoreInformation.Target;
-        public static string Win8Miame = "0.0.0" + CoreInformation.Target;
-        public static string Win8Rio = "1.1.0" + CoreInformation.Target;
-        public static string Win8Teresopolis = "2.0.0" + CoreInformation.Target;
-        #endregion
-
-        #region WEB
-        public static string WebLasVegas = "0.0.0" + CoreInformation.Target;
-        public static string WebPasadena = "0.0.0" + CoreInformation.Target;
-        public static string WebLyon = "0.0.0" + CoreInformation.Target;
-        public static string WebDouai = "0.0.0" + CoreInformation.Target;
-        public static string WebNice = "0.0.0" + CoreInformation.Target;
-        public static string WebMiame = "0.0.0" + CoreInformation.Target;
-        #endregion
-
-        #region IOS
-        public static string iOSLasVegas = "0.0.0" + CoreInformation.Target;
-        public static string iOSPasadena = "0.0.0" + CoreInformation.Target;
-        public static string iOSLyon = "0.0.0" + CoreInformation.Target;
-        public static string iOSDouai = "0.0.0" + CoreInformation.Target;
-        public static string iOSNice = "0.0.0" + CoreInformation.Target;
-        public static string iOSMiame = "0.0.0" + CoreInformation.Target;
-        #endregion
-
-        #region ANDROID
-        public static string AndroidLasVegas = "0.0.0" + CoreInformation.Target;
-        public static string AndroidPasadena = "0.0.0" + CoreInformation.Target;
-        public static string AndroidLyon = "0.0.0" + CoreInformation.Target;
-        public static string AndroidDouai = "0.0.0" + CoreInformation.Target;
-        public static string AndroidNice = "0.0.0" + CoreInformation.Target;
-        public static string AndroidMiame = "0.0.0" + CoreInformation.Target;
+        public static string Win8LasVegas = "1.1." + CoreInformation.Build + " _discontinued_";
+        public static string Win8Pasadena = "1.1." + CoreInformation.Build + " _discontinued_";
+        public static string Win8Lyon = "2.2." + CoreInformation.Build;
+        public static string Win8Douai = "1.1." + CoreInformation.Build;
+        public static string Win8Nice = "1.1." + CoreInformation.Build;
+        public static string Win8Miame = "0.0." + CoreInformation.Build;
+        public static string Win8Rio = "1.1." + CoreInformation.Build + " _discontinued_";
+        public static string Win8Teresopolis = "2.0." + CoreInformation.Build;
         #endregion
     }
 }
